d0f423bb16fe53f457abf45ceb904308033f45d3
Angger Priyardhan Putro (Laptop HP) <anggerpputro@gmail.com>
2018-02-18 14:22:48 +0700

    Looping

---
+++ app2.js
@@ -0,0 +1,10 @@
+new Vue({
+	el: '#app',
+	data: {
+		persons: ['Angger', 'Angie', 'Angel'],
+		nations: [
+			{name:'Indonesia', capital:'Jakarta'},
+			{name:'Malaysia', capital:'Kuala Lumpur'},
+		]
+	}
+});
